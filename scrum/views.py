ed354d5fa173cf6467190ef8849daedbf22b2911
Ana-Niko-byte <ananikolayenia@gmail.com>
2024-04-14 12:52:19 +0100

    feat: wire up scrum home view

--- scrum/views.py
+++ scrum/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def scrum_hello(response):
+    return HttpResponse('Hello from the Scrum Home page!')
--- zenlist/urls.py
+++ zenlist/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from scrum.views import scrum_hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', scrum_hello, name='scrum')
 ]
