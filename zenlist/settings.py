d7d5e57611e59cc879e7a0c494fd0c29796e4693
Ana-Niko-byte <ananikolayenia@gmail.com>
2024-04-19 13:12:45 +0100

    debug: reset secret key orig

--- zenlist/settings.py
+++ zenlist/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = 'django-insecure-z^r7t0*3$vgeswi4i9l7ij4d8wwo0o+r@2&u7fbc(%)9=5vxh&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
