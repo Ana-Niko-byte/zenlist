ac4ef574e92b72baacc17cab8271199e39db1da8
Ana-Niko-byte <ananikolayenia@gmail.com>
2024-04-24 19:21:03 +0200

    feat: add workspace view + adjust user authentication

--- workspace/urls.py
+++ workspace/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('workspaces/', views.WorkspaceListView.as_view(), name='spaces'),
+    path('<slug:slug>/', views.workspace_detail, name='full_workspace'),
 ]
--- workspace/views.py
+++ workspace/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Workspace
 
@@ -7,17 +7,43 @@ class WorkspaceListView(generic.ListView):
     template_name = 'workspace/workspace_list.html'
     context_object_name = 'spaces'
 
-# def WorkspaceList(request):
-#     model = Workspace
-#     space = Workspace.objects.all().first()
-#     template_name = 'workspace/workspace_list.html'
-
-#     context = {
-#         'space' : space,
-#         }
-    
-#     return render(
-#         request,
-#         "workspace/workspace_list.html",
-#         context
-#     )
+def workspace_detail(request, slug):
+    """
+    Display an individual instance of a workspace with tasks.
+    """
+    queryset = Workspace.objects.all()
+    workspace = get_object_or_404(queryset, slug=slug)
+    return render(
+        request,
+        "workspace/workspace_detail.html",
+        {
+            'workspace': workspace
+        },
+    )
+    pass
+    # queryset = Workspace.objects.filter(status=1)
+    # post = get_object_or_404(queryset, slug=slug)
+    # comments = post.comments.all().order_by("-created_on")
+    # comment_count = post.comments.filter(approved=True).count()
+    # if request.method == "POST":
+    #     comment_form = CommentForm(data=request.POST)
+    #     if comment_form.is_valid():
+    #         comment = comment_form.save(commit=False)
+    #         comment.author = request.user
+    #         comment.post = post
+    #         comment.save()
+    #         messages.add_message(
+    #     request, messages.SUCCESS,
+    #     'Comment submitted and awaiting approval'
+    # )
+    # comment_form = CommentForm()
+    # return render(
+    #     request,
+    #     "blog/post_detail.html",
+    #     {
+    #         "post": post,
+    #         "comments": comments,
+    #         "comment_count": comment_count,
+    #         "comment_form": comment_form,
+    #     },
+    # )
