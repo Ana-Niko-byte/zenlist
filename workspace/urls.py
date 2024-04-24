07959f3e6491a5be26d42559f09622bfa392efba
Ana-Niko-byte <ananikolayenia@gmail.com>
2024-04-24 19:24:07 +0200

    refactor: adjust workspace url configuration

--- workspace/urls.py
+++ workspace/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('workspaces/', views.WorkspaceListView.as_view(), name='spaces'),
+    path('', views.WorkspaceListView.as_view(), name='spaces'),
     path('<slug:slug>/', views.workspace_detail, name='full_workspace'),
 ]
--- zenlist/urls.py
+++ zenlist/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', HelloScrum, name='hello'),
-    path('', include('workspace.urls'), name='space-urls'),
+    path('workspaces/', include('workspace.urls'), name='space-urls'),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
