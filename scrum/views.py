6ba5a261178938b62ec7a7328ac5cc6d7ad9959f
Ana-Niko-byte <ananikolayenia@gmail.com>
2024-05-06 12:28:39 +0200

    feat: add reviews to Scrum

--- scrum/views.py
+++ scrum/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
-from .models import Scrum, Feature
+from .models import Scrum, Feature, Review
 
 def HelloScrum(request):
     scrum = Scrum.objects.all().first()
     feature = Feature.objects.all()
+    all_reviews = Review.objects.all()
+    five_stars = Review.objects.filter(rating='★★★★★')
+    four_stars = Review.objects.filter(rating='★★★★☆')
     context = {
         'hello' : scrum,
         'feature' : feature,
+        'all_reviews': all_reviews,
+        'five_star': five_stars,
+        'four_star': four_stars,
         }
     
     return render(
