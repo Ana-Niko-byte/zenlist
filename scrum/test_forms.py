224945d886c07ad11aa9ed1d5fda2f01c40a57f0
Ana Niko <ananikolayenia@gmail.com>
2024-06-04 10:45:33 +0000

    test: review form fields + login

--- scrum/test_forms.py
+++ scrum/test_forms.py
@@ -1,8 +1,9 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.core import mail
 from django.urls import reverse
 from django.conf import settings
 from .forms import *
+from django.contrib.auth.models import User
 
 
 class TestContactForm(TestCase):
@@ -206,3 +207,126 @@ class EmailTest(TestCase):
         self.assertIn("ananikolayenia@gmail.com", mail.outbox[0].to)
         self.assertIn("john.smith@gmail.com", mail.outbox[0].reply_to)
         self.assertRedirects(response, '/', status_code=302)
+
+
+class TestReviewForm(TestCase):
+    '''
+    A class for testing the ReviewForm Model is only visible to
+    logged in users, and that all fields are filled out as expected.
+
+    Methods:
+    def setUp():
+        As the review form is only visible to logged in users, setUp
+        simulates a logged in user using the test client object.
+
+        Note: More information on this can be found in the Django
+        documentation, using CTRL + F and 'test client' in this link:
+        https://docs.djangoproject.com/en/dev/topics/testing/tools/
+
+    def test_author_is_User():
+        This test verifies that the 'author' field of the review form is
+        automatically filled as the logged in User. In this test, this is
+        set manually as 'ananiko', as per the login simulation username
+        in setUp. This field is not available to end users for manual input.
+
+    def test_job_industry_is_required():
+        This test verifies that a populated review form which is missing a
+        job_industry input is not submitted, and that the error stems from
+        the missing job_industry field input.
+
+    def test_rating_is_required():
+        This test verifies that a populated review form which is missing a
+        rating input is not submitted, and that the error stems from the
+        missing rating field input.
+
+    def test_review_is_required():
+        This test verifies that a populated contact form which is missing a
+        review input is not submitted, and that the error stems from the
+        missing review field input.
+    '''
+    def setUp(self):
+        '''
+        Sets up a login simulation for the review form.
+        This form is only visible to logged in users on the home page.
+        '''
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='ananiko',
+            password='test-password'
+        )
+
+    def test_author_is_User(self):
+        '''
+        Creates a filled review form instance where review.author is
+        assigned manually as 'ananiko', as per the login simulation username
+        in setUp. This field is not available to end users for manual input.
+        Asserts the review.author field is the User.
+        Asserts the form is valid if the review.author field is User,
+        and if all fields have been filled out as expected.
+        '''
+        review_form = ReviewForm({
+            'author' : 'ananiko',
+            'job_industry' : 'Finance',
+            'rating': '★★★★☆',
+            'review': 'Great app!'
+        })
+        self.assertTrue('author', self.user)
+        self.assertTrue(review_form.is_valid())
+
+    def test_job_industry_is_required(self):
+        '''
+        Creates a filled review form instance, with an omitted job_industry
+        field.
+        Asserts the form is invalid if the job_industry field is empty.
+        Asserts the error stems from the empty job_industry field.
+        Asserts the invoked error is the expected error.
+        '''
+        review_form = ReviewForm({
+            'job_industry' : '',
+            'rating': '★★★★☆',
+            'review': 'Great app!'
+        })
+        self.assertFalse(review_form.is_valid())
+        self.assertIn('job_industry', review_form.errors.keys())
+        self.assertEqual(
+            review_form.errors['job_industry'][0],
+            'This field is required.'
+        )
+
+    def test_rating_is_required(self):
+        '''
+        Creates a filled review form instance, with an omitted rating field.
+        Asserts the form is invalid if the rating field is empty.
+        Asserts the error stems from the empty rating field.
+        Asserts the invoked error is the expected error.
+        '''
+        review_form = ReviewForm({
+            'job_industry' : 'Finance',
+            'rating': '',
+            'review': 'Great app!'
+        })
+        self.assertFalse(review_form.is_valid())
+        self.assertIn('rating', review_form.errors.keys())
+        self.assertEqual(
+            review_form.errors['rating'][0],
+            'This field is required.'
+        )
+
+    def test_review_is_required(self):
+        '''
+        Creates a filled review form instance, with an omitted review field.
+        Asserts the form is invalid if the review field is empty.
+        Asserts the error stems from the empty review field.
+        Asserts the invoked error is the expected error.
+        '''
+        review_form = ReviewForm({
+            'job_industry' : 'Finance',
+            'rating': '★★★★☆',
+            'review': ''
+        })
+        self.assertFalse(review_form.is_valid())
+        self.assertIn('review', review_form.errors.keys())
+        self.assertEqual(
+            review_form.errors['review'][0],
+            'This field is required.'
+        )
