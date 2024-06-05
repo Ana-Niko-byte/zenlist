973b0ca94cc685202f5714b96d6db4eebe40cf89
Ana Niko <ananikolayenia@gmail.com>
2024-06-05 08:14:55 +0000

    test: task form

--- workspace/test_forms.py
+++ workspace/test_forms.py
@@ -4,11 +4,12 @@ from django.conf import settings
 from .forms import *
 from django.contrib.auth.models import User
 
+
 class WorkspaceFormTest(TestCase):
     '''
     A class for testing the workspace form associated with
-    the Workspace Model. This form creates a new workspace 
-    instance if specific user inputs are correctly placed 
+    the Workspace Model. This form creates a new workspace
+    instance if specific user inputs are correctly placed
     in the form fields.
 
     Methods:
@@ -20,7 +21,6 @@ class WorkspaceFormTest(TestCase):
     def test_form_is_valid():
         This test verifies that a correctly filled out workspace form is
         successfully submitted.
-
     '''
     def test_title_is_required(self):
         '''
@@ -48,3 +48,152 @@ class WorkspaceFormTest(TestCase):
             'title': 'Testing Workspace Addition'
             })
         self.assertTrue(ws_form.is_valid())
+
+
+class TestTaskForm(TestCase):
+    '''
+    A class for testing the task form associated with
+    the Task Model. This form creates a new task
+    instance if specific user inputs are correctly placed
+    in the form fields.
+
+    Methods:
+    def test_title_is_required():
+
+
+    def test_form_is_valid():
+        This test verifies that a correctly filled out task form is
+        successfully submitted.
+    '''
+
+    def setUp(self):
+        '''
+        Sets up a login simulation for the task form to create a user.
+        Creates a workspace object instance for testing correct assignment.
+        '''
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='ananiko',
+            password='test-password'
+        )
+        self.workspace = Workspace.objects.create(
+            id=4,
+            title='headphones',
+            creator=self.user
+        )
+
+    def test_name_is_required(self):
+        '''
+        Creates a partially filled task form instance.
+        Asserts the form is invalid if the name field is empty.
+        Asserts the error stems from the empty name field.
+        Asserts the invoked error is the expected error.
+        '''
+        task_form = TaskForm({
+            'name': '',
+            'creator': self.user,
+            'workspace': 'headphones',
+            'priority': 'Minor',
+            'status': 'To Do',
+            'due_date': '2024-06-21',
+            'approved': False
+        })
+
+        self.assertFalse(task_form.is_valid())
+        self.assertIn('name', task_form.errors.keys())
+        self.assertEqual(
+            task_form.errors['name'][0],
+            'This field is required.'
+        )
+
+    def test_creator_is_User(self):
+        '''
+        Creates a filled task form instance where task.creator is
+        assigned manually as 'ananiko', as per the login simulation username
+        in setUp. This field is not available to end users for manual input.
+
+        Asserts the task.creator field is the User.
+        Asserts the form is valid if the task.creator field is User,
+        and if all fields have been filled out as expected.
+        '''
+        task_form = TaskForm({
+            'name': 'Test Task',
+            'creator': 'ananiko',
+            'workspace': 'headphones',
+            'priority': 'Minor',
+            'status': 'To Do',
+            'due_date': '2024-06-21',
+            'approved': False
+        })
+        self.assertTrue('creator', self.user)
+        self.assertTrue(task_form.is_valid())
+
+    def test_workspace_is_correctly_assigned(self):
+        '''
+        Creates a filled task form instance where task.workspace is
+        assigned manually as 'headphones', as per the workspace instance
+        in setUp. This field is not available to end users for manual input.
+
+        Asserts the task.workspace field is the workspace instance.
+        Asserts the form is valid if the task.workspace field matches
+        the expected instance, and if all other fields have been filled out.
+        '''
+        task_form = TaskForm({
+            'name': 'Test Task',
+            'creator': self.user,
+            'workspace': 'headphones',
+            'priority': 'Minor',
+            'status': 'To Do',
+            'due_date': '2024-06-21',
+            'approved': False
+        })
+        self.assertTrue('workspace', self.workspace)
+        self.assertTrue(task_form.is_valid())
+
+    def test_priority_is_required(self):
+        '''
+        Creates a partially filled task form instance.
+        Asserts the form is invalid if the priority field is empty.
+        Asserts the error stems from the empty priority field.
+        Asserts the invoked error is the expected error.
+        '''
+        task_form = TaskForm({
+            'name': 'Test Task',
+            'creator': self.user,
+            'workspace': 'headphones',
+            'priority': '',
+            'status': 'To Do',
+            'due_date': '2024-06-21',
+            'approved': False
+        })
+
+        self.assertFalse(task_form.is_valid())
+        self.assertIn('priority', task_form.errors.keys())
+        self.assertEqual(
+            task_form.errors['priority'][0],
+            'This field is required.'
+        )
+
+    def test_status_is_required(self):
+        '''
+        Creates a partially filled task form instance.
+        Asserts the form is invalid if the status field is empty.
+        Asserts the error stems from the empty status field.
+        Asserts the invoked error is the expected error.
+        '''
+        task_form = TaskForm({
+            'name': 'Test Task',
+            'creator': self.user,
+            'workspace': 'headphones',
+            'priority': 'Minor',
+            'status': '',
+            'due_date': '2024-06-21',
+            'approved': False
+        })
+
+        self.assertFalse(task_form.is_valid())
+        self.assertIn('status', task_form.errors.keys())
+        self.assertEqual(
+            task_form.errors['status'][0],
+            'This field is required.'
+        )
