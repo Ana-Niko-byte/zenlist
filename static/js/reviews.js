856c618e817cef8738009c5984f4df59721fe3b5
Ana Niko <ananikolayenia@gmail.com>
2024-06-07 16:14:36 +0000

    feat: sorting functionality (best, lowest) + styling adjustment

--- static/js/reviews.js
+++ static/js/reviews.js
@@ -1,27 +1,75 @@
 // Sorting Functionality 
 
+function determineRatingLength(retrievedArray){
+    /**
+     * Iterates over retrieved array to determine the length of the rating dataset attribute 'highestRate'.
+     * Reassigns integer value to dataset highestRate.
+     * 
+     * Arguments:
+     * retrievedArray : array, array for determining length of highestRate dataset value.
+     */
+    for (const item of retrievedArray){
+        // Removes all empty stars ('★★★★', ''), joins at '' ('★★★★'), find the length (4). 
+        let reviewLength = item.getAttribute("data-highestRate").split('☆').join('').length;
+        // Sets the value of dataset highestRate to the reviewLength.
+        item.dataset.highestRate = reviewLength;
+    };
+}
+
+function resetValues(retrievedArray){
+    /**
+     * This function is used to re-append sorted array items to the review-box.
+     * 
+     * Arguments:
+     * retrievedArray : array, the array to be iterated over.
+     * 
+     * Method:
+     * Retrieves the review-box by Id.
+     * Empties the review-box.
+     * Iterates through the array and appends each item to the review-box.
+     */
+    document.getElementById("review-box").innerHTML = '';
+    for (const item of retrievedArray){
+        document.getElementById("review-box").append(item);
+    };
+}
+
 const selectField = document.getElementById("review-filter").addEventListener("change", function(){
     /**
+     * On change event, sorts values based on selection.
      * 
+     * Retrieves the selection value.
+     * Retrieves the array of items to be sorted by class value 'zenlist-reviews'.
+     * 
+     * Change value of dataset highestRate from symbols to integer value indicating quantity of filled stars.
      * 
      */
     let selectedOption = this.value;
     let reviewItemsArray = Array.from(document.getElementsByClassName("zenlist-review"));
-    let ratings = []
 
-    for (const item of reviewItemsArray){
-        let arrayedStars = item.getAttribute("data-highest-rate").split('☆').join('').split('');
-        const itemId = item.getAttribute("data-review-id");
-        ratings.push([itemId, arrayedStars.length]);
-    };
+    // Sets highestRate value to numeric.
+    determineRatingLength(reviewItemsArray);
 
+    // Filtering
     if (selectedOption === 'by best'){
-        // Sort by second item in sub array in ascending order.
-        ratings.sort((a, b) => b[1] - a[1]);
+        reviewItemsArray.sort(function(a,b) {
+            // Parsing as dataset highestRate is set to String.
+            let reviewLengthA = parseInt(a.dataset.highestRate);
+            let reviewLengthB = parseInt(b.dataset.highestRate);
+
+            return reviewLengthB - reviewLengthA;
+        });
+        // Clear existing order + set to sorted array.
+        resetValues(reviewItemsArray);
 
     } else if (selectedOption === 'by lowest'){
-        // Sort by second item in sub array in descending order.
-        ratings.sort((a, b) => a[1] - b[1]);
+        reviewItemsArray.sort(function(a,b) {
+            let reviewLengthA = parseInt(a.dataset.highestRate);
+            let reviewLengthB = parseInt(b.dataset.highestRate);
+
+            return reviewLengthA - reviewLengthB;
+        });
+        resetValues(reviewItemsArray);
 
     } else if (selectedOption === 'by recent'){
         // Sort by date added (in view?).
@@ -29,23 +77,23 @@ const selectField = document.getElementById("review-filter").addEventListener("c
     };
 });
 
+
 // Delete Functionality
-const deleteReviewModal = new bootstrap.Modal(document.getElementById("deleteReviewModal"));
 const deletes = document.getElementsByClassName("btn-delete");
-const deleteReviewConfirm = document.getElementById("deleteReviewConfirm");
 
 /**
  * Initializes deletion functionality for the provided delete buttons.
  * 
  * For each button in the `deletes` array:
- * - Onclick, retrieves the associated task's ID.
- * - Updates the `deleteConfirm` link's href to point to the 
- * deletion endpoint for the specific comment.
- * - Displays a confirmation modal (`deleteModal`) to prompt 
- * the user for confirmation before deletion.
+ * Onclick, retrieves the associated review's ID.
+ * Updates the `deleteReviewConfirm` link's href to point to the deletion endpoint for the specific review.
+ * Displays a confirmation modal (`deleteReviewModal`) to prompt the user for confirmation before deletion.
  */
 for (let button of deletes) {
     button.addEventListener("click", (e) => {
+        const deleteReviewModal = new bootstrap.Modal(document.getElementById("deleteReviewModal"));
+        const deleteReviewConfirm = document.getElementById("deleteReviewConfirm");
+
         let reviewId = e.target.getAttribute("data-review-id");
         deleteReviewConfirm.href = `delete-review/${reviewId}`;
         deleteReviewModal.show();
--- static/js/tasks.js
+++ static/js/tasks.js
@@ -7,11 +7,9 @@ const deleteConfirm = document.getElementById("deleteConfirm");
  * Initializes deletion functionality for the provided delete buttons.
  * 
  * For each button in the `deletes` array:
- * - Onclick, retrieves the associated task's ID.
- * - Updates the `deleteConfirm` link's href to point to the 
- * deletion endpoint for the specific comment.
- * - Displays a confirmation modal (`deleteModal`) to prompt 
- * the user for confirmation before deletion.
+ * Onclick, retrieves the associated task's ID.
+ * Updates the `deleteConfirm` link's href to point to the deletion endpoint for the specific task.
+ * Displays a confirmation modal (`deleteModal`) to prompt the user for confirmation before deletion.
  */
 for (let button of deletes) {
     button.addEventListener("click", (e) => {
@@ -36,8 +34,9 @@ const submit = document.getElementById("submitButton");
 
 /**
  * Iterates through all edit buttons and appends a 'click' event listener.
+ * 
  * taskId : retrieves the id of the task that has been clicked.
- * task- : retrieves the value of the 'data--' attributes attached to the edit button.
+ * task- : retrieves the value of dataset attributes attached to the edit button.
  * 
  * Formats the date value returned from attribute: data-edit-date to the ISO recognised format in HTML YYYY-mm-dd.
  * Assigns retrieved attributes to the values of the relevant form fields.
